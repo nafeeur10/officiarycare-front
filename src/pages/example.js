a72667398f39946ced14790d603525e9330993b5
Nafeeur Rahman <nafeeur.kuet.cse6@gmail.com>
2022-09-21 10:37:11 +0600

    Example Page added

---
+++ src/pages/example.js
@@ -0,0 +1,13 @@
+const ExamplePage = () => {
+    const { logout, user } = useAuth({ middleware: 'auth' })
+
+    return (
+        <>
+            <p>{user?.name}</p>
+
+            <button onClick={logout}>Sign out</button>
+        </>
+    )
+}
+
+export default ExamplePage
